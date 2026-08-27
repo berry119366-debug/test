0f7fcbb937a39746a293b65b71b9aba21847a2ad
Berry119366 <berry119366@mcpsva.org>
2026-08-27 10:21:16 -0400

    A

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+"""
+filename:main.py
+Author: Raynor Matos, Berry
+Created: 8/27/2026
+Instructor: Burgess
+"""
 x=input("do you have any prior experience with teaching")
+print(x)
 age=input("are you a confident speaker in front of a group of people")
+print(age)
 w=input("will you show up on time everyday")
+print(w)
 d=input("will you follow the teacher guideline")
+print(d)
 e=input("will you take care of students as its your responsibility")
-print("x")
-print("age")
-print("w")
-print("d")
-print("e")
+print(e)
