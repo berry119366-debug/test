9e9a5c4d6911ae465c474a49066f96d1805d274a
Berry119366 <berry119366@mcpsva.org>
2026-08-31 11:02:37 -0400

    A

---
+++ Madlib.py
@@ -0,0 +1,18 @@
+"""
+file:Madlib.py
+Author: Raynor Matos Berry
+Created: 8/31/2026
+Instructor: Burgess
+"""
+w1=input("groom:")
+w2=input("bride:")
+w3=input("noun:")
+w4=input("noun:")
+w5=input("adjective:")
+w6=input("noun:")
+w7=input("noun:")
+w8=input("adjective:")
+w9=input("place:")
+w10=input("past tense verb:")
+w11=input("number:")
+w12=input("adjective:")
